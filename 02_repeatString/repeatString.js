e2557143819820600c95eddd873f07e6663ae1d6
Felix S <116848884+latsonj@users.noreply.github.com>
2023-02-06 11:08:41 +1300

    Finish 02_reapeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  if (num < 0) {
+    let negative = string.replace(string, "ERROR");
+    return negative;
+  } else if (num === 0) {
+    let zeroString = "";
+    return zeroString;
+  } else {
+    let result = string;
+    for (let i = 1; i < num; i++) {
+      result += string;
+    }
+    return result;
+  }
+}
 
 // Do not edit below this line
-module.exports = repeatString;
+ module.exports = repeatString;
