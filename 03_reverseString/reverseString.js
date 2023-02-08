1f14d258b3092819c1ef015fc2ec024ac9243de1
Felix S <116848884+latsonj@users.noreply.github.com>
2023-02-08 19:08:13 +1300

    Complete 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const textArr = string.split("");
+  const reversedArr = textArr.reverse();
 
-};
+  let arrToText = "";
+
+  for (let i = 0; i <= (reversedArr.length - 1); i++) {
+    arrToText += reversedArr[i];
+  }
+    return arrToText;
+}
+
+/* Simple answer (join() method parameter is separator):
+return string.split("").reverse().join(""); */
 
 // Do not edit below this line
 module.exports = reverseString;
