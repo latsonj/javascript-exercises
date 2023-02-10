20bda9ef55c14ecd2e0ecf0dd7fe235674485f3c
Felix S <116848884+latsonj@users.noreply.github.com>
2023-02-10 16:59:28 +1300

    Finish 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
 
+  remove: for (let i = 0; i < values.length; i++) {
+    let index = array.indexOf(values[i]);
+
+    if (index < 0) {
+      continue remove;
+    }
+    array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
