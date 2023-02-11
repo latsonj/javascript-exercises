4f2a674caa7dbaf9beed3221c0902a8baba41c39
Felix S <116848884+latsonj@users.noreply.github.com>
2023-02-11 21:57:18 +1300

    Finish 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
-};
+  if ((num1 < 0 || num2 < 0) || (typeof num1 !== "number") || (typeof num2 !== "number")) {
+    return "ERROR";
+
+  } else if (num1 > num2) {
+    let finalSum = num2;
+    for (let i = num2; i < num1; i++) {
+      finalSum += i + 1;
+  }
+  return finalSum;
+
+  } else if (num1 < num2 ) {
+    let finalSum = num1;
+    for (let i = num1; i < num2; i++) {
+        finalSum += i + 1;
+  }
+  return finalSum;
+  };
+}
 
 // Do not edit below this line
 module.exports = sumAll;
