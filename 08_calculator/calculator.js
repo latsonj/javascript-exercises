bf4c60209e4e260f8c91ce247693ef2485404245
Felix S <116848884+latsonj@users.noreply.github.com>
2023-06-09 21:42:36 +1200

    Complete 08_calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  } else {
+    return array.reduce((prev, current) => prev + current);
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return numbers.reduce((prev, current) => prev * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let array = [];
+  for (let i = 1; i <= num; i++) {
+    array.push(i);
+  }
+  if (array.length > 0) {
+    return array.reduce((prev, current) => prev * current);
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
