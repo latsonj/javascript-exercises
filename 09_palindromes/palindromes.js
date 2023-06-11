a789020331cf4d5c21e301be8f5de0d236cda240
Felix S <116848884+latsonj@users.noreply.github.com>
2023-06-11 16:14:51 +1200

    Complete 09_palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let comparisonString = str.replaceAll(" ", "").replaceAll("!", "").replaceAll(",", "").replaceAll(".", "").toLowerCase();
+  let reverseString = comparisonString.split("").reverse().join("");
+  
+  if (comparisonString === reverseString) {
+    return true;
+  } else return false;
 };
 
 // Do not edit below this line
