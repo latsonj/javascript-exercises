6d6de8c5814c3ea7f55693da461dccbbb1528911
Felix S <116848884+latsonj@users.noreply.github.com>
2023-06-11 17:21:50 +1200

    Complete 10_fibonacci exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let fibonacciArray = [1, 1];
+  if (n < 0) {
+    return "OOPS"
+  }
+  for (let i = 2; i < n; i++) {
+    fibonacciArray.push((fibonacciArray[i-2]) + (fibonacciArray[i-1]))
+  }
+  return fibonacciArray[fibonacciArray.length - 1];
 };
 
 // Do not edit below this line
